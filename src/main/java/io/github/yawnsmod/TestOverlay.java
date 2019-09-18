ac8f43abc3f9266bb4e6ca139c4fb9c57735a1c7
AlexApps99 <alex.apps99@gmail.com>
2019-09-18 21:07:48 +1200

    Added anti-overlay, hides helmet, portal swirl (not nausea), fire, water texture. Todo: - water/lava fog (see http://jabelarminecraft.blogspot.com/p/minecraft-modding-creating-colored-fog.html) - nausea/blindness effect

--- src/main/java/io/github/yawnsmod/TestOverlay.java
+++ src/main/java/io/github/yawnsmod/TestOverlay.java
@@ -1,16 +1,50 @@
 package io.github.yawnsmod;
 
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.RenderBlockOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
-import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class TestOverlay
 {
+	@SubscribeEvent(receiveCanceled=true)
+	public void beforeRenderGui(RenderGameOverlayEvent.Pre event)
+	{
+		switch (event.getType()) {
+		case HELMET:
+			event.setCanceled(true);
+			break;
+		case PORTAL:
+			event.setCanceled(true);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	@SubscribeEvent(receiveCanceled=true)
+	public void beforeRenderBlockOverlay(RenderBlockOverlayEvent event)
+	{
+		switch (event.getOverlayType()) {
+		case FIRE:
+			event.setCanceled(true);
+			break;
+		//case BLOCK:
+		//	event.setCanceled(true);
+		//	break;
+		case WATER:
+			event.setCanceled(true);
+			break;
+		default:
+			break;
+		}
+	}
+	
 	@SubscribeEvent
 	public void onRenderGui(RenderGameOverlayEvent.Post event)
 	{
-		if (event.getType() != ElementType.ALL) return; //EXPERIENCE
-		new TPSMeter(Minecraft.getMinecraft(), event);
+		if (event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
+			new TPSMeter(Minecraft.getMinecraft(), event);
+		}
 	}
 }
