4fce215c0f42e09572e3ada22485192d1fe0854b
AlexApps99 <alex.apps99@gmail.com>
2019-09-24 10:47:32 +1200

    Helping the project along to compile

--- src/main/java/io/github/yawnsmod/Imgui.java
+++ src/main/java/io/github/yawnsmod/Imgui.java
@@ -1,43 +1,43 @@
-package io.github.yawnsmod;
+// package io.github.yawnsmod;
 
-import imgui.ImGui;
-import imgui.ImguiKt;
-import imgui.imgui.Context;
-import imgui.impl.gl.ImplGL3;
-import imgui.impl.glfw.ImplGlfw;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
-//import net.minecraft.util.text.ITextComponent;
-import uno.glfw.GlfwWindow;
+// import imgui.ImGui;
+// import imgui.ImguiKt;
+// import imgui.imgui.Context;
+// import imgui.impl.gl.ImplGL3;
+// import imgui.impl.glfw.ImplGlfw;
+// import net.minecraft.client.Minecraft;
+// import net.minecraft.client.gui.GuiScreen;
+// import net.minecraftforge.client.event.RenderGameOverlayEvent;
+// //import net.minecraft.util.text.ITextComponent;
+// import uno.glfw.GlfwWindow;
 
-public class Imgui extends GuiScreen {
-	public Imgui(Minecraft mc, RenderGameOverlayEvent.Post event)
-    {
+// public class Imgui extends GuiScreen {
+// 	public Imgui(Minecraft mc, RenderGameOverlayEvent.Post event)
+//     {
 
-	    ImGui imgui = ImGui.INSTANCE;
+// 	    ImGui imgui = ImGui.INSTANCE;
 	
-	    ImplGL3 implGl3;
-	    ImplGlfw implGlfw;
+// 	    ImplGL3 implGl3;
+// 	    ImplGlfw implGlfw;
 
-        ImguiKt.MINECRAFT_BEHAVIORS = true;
-        GlfwWindow window = GlfwWindow.from(Minecraft.getInstance().mainWindow.getHandle());
-        window.makeContextCurrent();
-        new Context();
-        implGlfw = new ImplGlfw(window, false, null);
-        implGl3 = new ImplGL3();
-        implGl3.newFrame();
-        implGlfw.newFrame();
+//         ImguiKt.MINECRAFT_BEHAVIORS = true;
+//         GlfwWindow window = GlfwWindow.from(Minecraft.getInstance().mainWindow.getHandle());
+//         window.makeContextCurrent();
+//         new Context();
+//         implGlfw = new ImplGlfw(window, false, null);
+//         implGl3 = new ImplGL3();
+//         implGl3.newFrame();
+//         implGlfw.newFrame();
 
-        imgui.newFrame();
+//         imgui.newFrame();
 
-        //Render things here
+//         //Render things here
 
-        imgui.text("Hello Minecraft!");
+//         imgui.text("Hello Minecraft!");
 
-        //and stop here
+//         //and stop here
 
-        implGl3.renderDrawData(imgui.getDrawData());
+//         implGl3.renderDrawData(imgui.getDrawData());
 
-    }
-}
+//     }
+// }
